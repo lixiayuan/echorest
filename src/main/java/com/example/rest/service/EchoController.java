79f9f8c320f29c468ba2e1ff8a1c2efd3bc1fb15
lixia_yuan <lixia.yuan@oracle.com>
2020-08-14 22:39:06 +0800

    update getIpAddr func

--- src/main/java/com/example/rest/service/EchoController.java
+++ src/main/java/com/example/rest/service/EchoController.java
@@ -3,17 +3,57 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class EchoController {
+    private static final String UNKNOWN = "unknown";
+    private static final String LOCALHOST = "127.0.0.1";
+    private static final String SEPARATOR = ",";
 
+    public static String getIpAddr(HttpServletRequest request) {
+        System.out.println(request);
+        String ipAddress;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                if (LOCALHOST.equals(ipAddress)) {
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+
+            if (ipAddress != null && ipAddress.length() > 15) {
+                if (ipAddress.indexOf(SEPARATOR) > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress = "";
+        }
+        return ipAddress;
+    }
+    
     @PostMapping(path = "/echo", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public Echo echo(@RequestParam(value = "x", defaultValue = "Hello") String queryStringParams,
                      @RequestBody Greeting requestBody, HttpServletRequest request) {
-        String remoteAddr = request.getRemoteAddr();
+        String ip = getIpAddr(request);
         String useragent = request.getHeader("User-Agent");
-        Echo echo = new Echo(queryStringParams, requestBody,remoteAddr, useragent);
+        Echo echo = new Echo(queryStringParams, requestBody, ip, useragent);
         return echo;
     }
 
